522704814a33ff906e1d39b4ae4ca4446bd10ad1
ClaudioK5 <klaudio.asaro5@gmail.com>
2026-08-05 16:20:43 +0200

    Add files via upload

--- src/components/EnvisionLogo.tsx
+++ src/components/EnvisionLogo.tsx
@@ -1,12 +1,18 @@
+import { useTheme } from "../theme/ThemeProvider";
+
 type Props = {
   className?: string;
   size?: number;
 };
 
 export function EnvisionLogo({ className, size = 36 }: Props) {
+  const { theme } = useTheme();
+  const src =
+    theme === "girlie" ? "/envision-logo-girlie.png" : "/envision-logo.png";
+
   return (
     <img
-      src="/envision-logo.png"
+      src={src}
       alt=""
       className={className}
       width={size}
